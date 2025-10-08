d54c264d23f157879ce73de380873a4118f6ab30
janosdeak <markxjanosdeak@gmail.com>
2025-10-08 10:00:55 +0000

    Deploying to gh-pages from @ janosdeak/janosdeak.github.io@9043da2fc946fbbfa0fe72564aec4807c3617aae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -432,15 +432,15 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-my-second-short-story-got-published-accompanied-by-an-interview",
+          title: 'My second short story got published accompanied by an interview (🇭🇺)',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement-LK-szeptember/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
